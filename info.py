2ec394838dd5b7a6a648fa705663cc124e64da1c
Arun <86429222+Arun-TG@users.noreply.github.com>
2021-10-16 00:49:55 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,5 +3,5 @@ import os
 API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = os.environ.get("API_HASH", None)
 SESSION = os.environ.get("SESSION")
-ADMINS = set(int(x) for x in os.environ.get("ADMINS", "1420903394").split())
+ADMINS = set(int(x) for x in os.environ.get("ADMINS", "1914868167").split())
 TIME = os.environ.get("TIME", None)
